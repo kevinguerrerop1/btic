ce0d53393bf2875dc0b218d660f44f50f3d5b113
Kevin Guerrero <keeeevin11@gmail.com>
2024-03-08 18:22:48 -0300

    Add BD

---
+++ app3.sql
@@ -0,0 +1,109 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.28-MariaDB : Database - btic
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`btic` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `btic`;
+
+/*Table structure for table `articulos` */
+
+DROP TABLE IF EXISTS `articulos`;
+
+CREATE TABLE `articulos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(20) DEFAULT NULL,
+  `modelo` varchar(20) DEFAULT NULL,
+  `nroserie` varchar(20) DEFAULT NULL,
+  `idcat` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idcat` (`idcat`),
+  CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`idcat`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `articulos` */
+
+insert  into `articulos`(`id`,`marca`,`modelo`,`nroserie`,`idcat`) values (1,'Hp','4103','123kjasd',1),(2,'a','s','s',3);
+
+/*Table structure for table `categorias` */
+
+DROP TABLE IF EXISTS `categorias`;
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomcat` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `categorias` */
+
+insert  into `categorias`(`id`,`nomcat`) values (1,'Impresora'),(2,'Pc'),(3,'Proyector'),(4,'Toner');
+
+/*Table structure for table `prestamos` */
+
+DROP TABLE IF EXISTS `prestamos`;
+
+CREATE TABLE `prestamos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idart` int(11) DEFAULT NULL,
+  `ps` varchar(20) DEFAULT NULL,
+  `pe` int(11) DEFAULT NULL,
+  `idser` int(11) DEFAULT NULL,
+  `ubicacion` varchar(20) DEFAULT NULL,
+  `fchentrega` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idart` (`idart`),
+  KEY `idser` (`idser`),
+  KEY `pe` (`pe`),
+  CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`idart`) REFERENCES `articulos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`idser`) REFERENCES `servicios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `prestamos_ibfk_3` FOREIGN KEY (`pe`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `prestamos` */
+
+insert  into `prestamos`(`id`,`idart`,`ps`,`pe`,`idser`,`ubicacion`,`fchentrega`) values (5,1,'a',1,1,'a','08-03-2024');
+
+/*Table structure for table `servicios` */
+
+DROP TABLE IF EXISTS `servicios`;
+
+CREATE TABLE `servicios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomserv` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `servicios` */
+
+insert  into `servicios`(`id`,`nomserv`) values (1,'TIC'),(2,'Direccion');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(10) DEFAULT NULL,
+  `Apellido` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`Nombre`,`Apellido`) values (1,'Kevin','Guerrero');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
